bdf2dcfecd2b1537c8f86b5329c3b1668490827d
jackielu <jackielu@gmail.com>
2014-12-02 19:46:15 -0500

    working Leaflet in Skeleton boilerplate with sidebar derived from data

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Final project in progress for XSAVI-780
+
+This is the first attempt at the landCover project using Leaflet to add polygons and styles to the map and the Skeleton boilerplate, with sidebar elements derived from the data that highlight polygons on the map with hover.  No D3 has been incorporated yet.
--- js/script.js
+++ js/script.js
@@ -1,33 +1,102 @@
 //set up our map
 var map = L.map('map').setView([40.7056258,-73.97968], 10)
 
-		//Add basemap from mapbox
-		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-			maxZoom: 18,  //can't zoom in closer than 18
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'examples.map-20v6611k'
-			//not sure why the map was given an ID?  
-		}).addTo(map);
+		// //Add basemap from mapbox
+		// L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		// 	maxZoom: 18,  //can't zoom in closer than 18
+		// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		// 		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		// 		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		// }).addTo(map);
 
+
+var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+}).addTo(map);
 
 
 //function to create pop-ups and sidebar divs
-function makeMarkers(feature, layer){
-	console.log(feature);
+function makePopUps(feature, layer){
+	//console.log(feature);
 	//layer.bindPopup defines the pop-up value
 	layer.bindPopup(
 		"Neighborhood: <br>"
 		+ feature.properties.Alt_Name
 		);
+
+	//set up divs classed using the MINOR_DESC 
+	$('#geoList').append(
+		"<div class = 'geoListItem' id='"
+		+ feature.properties.NEIGH_CODE
+		+"'>"
+		+ feature.properties.Alt_Name
+		+"</div>"
+		)
 	}
 
-//ad data to the map
 
+
+function highlightMarker(geojsonLayer,thisPoly) {
+  geojsonLayer.eachLayer(function(marker) {
+		if(thisPoly==marker.feature.properties.NEIGH_CODE) {
+   		marker.setStyle({
+   			fillOpacity: 1,
+   			weight:1,
+   			color: 'steelblue'
+   			});
+   			//console.log(marker.options.fillOpacity);
+		} else {
+			marker.setStyle({
+				fillOpacity:.8,
+				weight:1,
+				color: '#cecece'
+				});
+		}
+  });
+}
+
+
+// get color depending on population density value
+function getColor(d) { //at some point in the future we will be calling out the value d from the dataset
+	return d > 80  ? '#00441b' :
+	       d > 60  ? '#238b45' :
+	       d > 40  ? '#74c476' :
+	       d > 20  ? '#c7e9c0' :
+	                  '#f7fcf5';
+}
+
+function style(feature) {//this function is being passed a feature and is returning a pile of style attributed
+	//console.log(feature);
+	return {
+		weight: 1,
+		opacity: 1,
+		color: '#cecece',
+		fillOpacity: 0.8,
+		fillColor: getColor(feature.properties.Can_P) //getColor was defined right up above. this statement says pick the color depending on the value in the density property of the us-states.js.  so feature.properties.density is being passed as 'd'
+	};
+}
+
+
+
+//add data to the map
 $.getJSON('data/landcoversumm.geojson', function(data) {
 	var geojsonLayer = L.geoJson(data.features, {
-		onEachFeature: makeMarkers,
+		style: style, //styled above
+		onEachFeature: makePopUps,
 	}).addTo(map);
+
+
+	$('.geoListItem')
+	.mouseenter(function(){
+		$(this).toggleClass('highlight');
+		var thisPoly = $(this).attr('id');
+		highlightMarker(geojsonLayer,thisPoly);
+	})
+	.mouseout(function(){
+		$(this).toggleClass('highlight');
+		var thisPoly;
+		highlightMarker(geojsonLayer,thisPoly);
+	})
 });
 
